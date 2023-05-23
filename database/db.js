4494cfdc837dbbec539984be451caf0656a110f4
mateogon <mateogon97@hotmail.com>
2023-05-22 20:45:10 -0400

    cambios

--- database/db.js
+++ database/db.js
@@ -2,7 +2,14 @@ const { Client } = require("pg");
 
 // Function to connect to the database
 async function connectToDatabase() {
-  const client = new Client();
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "optica",
+    password: "password",
+    port: 5432,
+  });
+
   await client.connect();
   console.log("Connected to the database");
   return client;
@@ -11,9 +18,12 @@ async function connectToDatabase() {
 // Function to execute a query
 async function executeQuery(query) {
   const client = await connectToDatabase();
-  const result = await client.query(query);
-  client.end();
-  return result.rows;
+  try {
+    const result = await client.query(query);
+    return result.rows;
+  } finally {
+    client.end();
+  }
 }
 
 module.exports = {
