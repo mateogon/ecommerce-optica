c29e56360a03df9d1d7225d9f2482479e201ce08
mateogon <mateogon97@hotmail.com>
2023-06-04 13:26:43 -0400

    ejs, elephantsql db, app, views

--- API/app.js
+++ API/app.js
@@ -1,166 +1,239 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pg = require('pg');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser"); // Don't forget to install it using npm install body-parser
 
-// Configuración de la conexión a la base de datos PostgreSQL
-const pool = new pg.Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "optica",
-    password: "password",
-    port: 5432,
-});
+const path = require("path");
+const {
+  query,
+  searchProducts,
+  searchProductsByNameOrDescription,
+  userLogin,
+  createUser,
+  updateUser,
+  addToCart,
+  getCotizations,
+  updateRecipe,
+  getUsuarios,
+  getProductos,
+  getCarritoComprasByUser,
+  getComprasByUser,
+  getInventario,
+  getPromociones,
+  getResenasByUser,
+  getNotificacionesByUser,
+  getMetodoPagoByUser,
+  getContactoByUser,
+  getHistorialComprasByUser,
+  getBusquedaProductoByUser,
+  getListaDeseosByUser,
+} = require("../database/database");
 
 const app = express();
-const path = require('path');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "..")));
 
-app.use(express.static(path.join(__dirname, '..')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+// Route handlers
+app.get("/", (req, res) => {
+  res.render("index", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
 });
 
-// Ruta para buscar productos por tipo de lente, tamaño, color y marca
-app.get('/productos', (req, res) => {
+app.get("/productos", (req, res) => {
+  res.render("productos", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
+});
+
+app.get("/carrito", (req, res) => {
+  res.render("carrito", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
+});
+
+app.get("/perfil", (req, res) => {
+  res.render("perfil", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
+});
+
+app.get("/receta", (req, res) => {
+  res.render("receta", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
+});
+
+app.get("/registro", (req, res) => {
+  res.render("registro", {
+    header: "partials/header",
+    session: req.session,
+    isUserLoggedIn: req.session && req.session.usuario,
+  });
+});
+app.get("/logout", (req, res) => {
+  // Clear the user session
+  req.session.usuario = null;
+
+  // Redirect to the login page or any other desired page
+  res.redirect("/login");
+});
+
+app.get("/productos", async (req, res) => {
   const { tipo_lente, color, marca } = req.query;
-  console.log(tipo_lente, color, marca);
-  let query = null;
-  if (tipo_lente == null || color == null || marca == null) {
-    query = {
-      text: 'SELECT * FROM Productos'
-    };
+  try {
+    const result = await searchProducts(tipo_lente, color, marca);
+    res.json(result);
+  } catch (error) {
+    console.error("Error al buscar productos:", error);
+    res.status(500).json({ mensaje: "Error al buscar productos" });
   }
-  else {
-    query = {
-      text: 'SELECT * FROM Productos WHERE tipo_lente = $1 AND color = $2 AND marca = $3',
-      values: [tipo_lente, color, marca],
-    };
-  }
-
-  pool.query(query, (error, result) => {
-    if (error) {
-      console.error('Error al buscar productos:', error);
-      res.status(500).json({ mensaje: 'Error al buscar productos' });
-    } else {
-      console.log(result.rows);
-      res.json(result.rows);
-    }
-  });
 });
 
-// Ruta para búsqueda específica a través de un buscador
-app.get('/productos/buscar', (req, res) => {
+app.get("/productos/buscar", async (req, res) => {
   const { q } = req.query;
-  const query = {
-    text: 'SELECT * FROM Productos WHERE nombre_producto ILIKE $1 OR descripcion ILIKE $1',
-    values: [`%${q}%`],
-  };
-
-  pool.query(query, (error, result) => {
-    if (error) {
-      console.error('Error al buscar productos:', error);
-      res.status(500).json({ mensaje: 'Error al buscar productos' });
-    } else {
-      res.json(result.rows);
-    }
-  });
+  try {
+    const result = await searchProductsByNameOrDescription(q);
+    res.json(result);
+  } catch (error) {
+    console.error("Error al buscar productos:", error);
+    res.status(500).json({ mensaje: "Error al buscar productos" });
+  }
 });
 
-// Ruta para inicio de sesión de un usuario
-app.post('/usuarios/login', (req, res) => {
+app.post("/usuarios/login", async (req, res) => {
   const { nombre_usuario, contrasena } = req.body;
-  const query = {
-    text: 'SELECT * FROM Usuarios WHERE nombre_usuario = $1 AND contrasena = $2',
-    values: [nombre_usuario, contrasena],
-  };
-
-  pool.query(query, (error, result) => {
-    if (error) {
-      console.error('Error al iniciar sesión:', error);
-      res.status(500).json({ mensaje: 'Error al iniciar sesión' });
-    } else if (result.rows.length === 0) {
-      res.status(401).json({ mensaje: 'Credenciales inválidas' });
+  try {
+    const result = await userLogin(nombre_usuario, contrasena);
+    if (result.length === 0) {
+      res.status(401).json({ mensaje: "Credenciales inválidas" });
     } else {
-      res.json({ mensaje: 'Inicio de sesión exitoso', usuario: result.rows[0] });
+      req.session.usuario = result[0]; // Store the user in the session
+      res.json({ mensaje: "Inicio de sesión exitoso", usuario: result[0] });
     }
-  });
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
+    res.status(500).json({ mensaje: "Error al iniciar sesión" });
+  }
 });
 
-// Ruta para modificación del perfil de un usuario
-app.put('/usuarios/:id', (req, res) => {
+app.post("/usuarios/register", async (req, res) => {
+  const { nombre_usuario, email, contrasena, tipo_usuario, nombre, apellido } =
+    req.body;
+  console.log("req:", {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    body: req.body,
+  });
+  try {
+    const newUser = await createUser(
+      nombre_usuario,
+      tipo_usuario,
+      contrasena,
+      email,
+      nombre,
+      apellido
+    );
+
+    if (newUser) {
+      req.session.usuario = newUser; // Store the user in the session
+      res.json({ mensaje: "Registro exitoso", usuario: newUser });
+    } else {
+      res.status(400).json({ mensaje: "Error al registrar" });
+    }
+  } catch (error) {
+    console.error("Error al registrar:", error);
+    res.status(500).json({ mensaje: "Error al registrar" });
+  }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
   const { id } = req.params;
-  const { nombre_usuario, tipo_usuario, contrasena, email, informacion_contacto } = req.body;
-  const query = {
-    text: 'UPDATE Usuarios SET nombre_usuario = $1, tipo_usuario = $2, contrasena = $3, email = $4, informacion_contacto = $5 WHERE id = $6',
-    values: [nombre_usuario, tipo_usuario, contrasena, email, informacion_contacto, id],
-  };
-
-  pool.query(query, (error) => {
-    if (error) {
-      console.error('Error al modificar el perfil:', error);
-      res.status(500).json({ mensaje: 'Error al modificar el perfil' });
-    } else {
-      res.json({ mensaje: 'Perfil actualizado correctamente' });
-    }
-  });
+  const {
+    nombre_usuario,
+    tipo_usuario,
+    contrasena,
+    email,
+    informacion_contacto,
+  } = req.body;
+  try {
+    await updateUser(
+      id,
+      nombre_usuario,
+      tipo_usuario,
+      contrasena,
+      email,
+      informacion_contacto
+    );
+    res.json({ mensaje: "Perfil actualizado correctamente" });
+  } catch (error) {
+    console.error("Error al modificar el perfil:", error);
+    res.status(500).json({ mensaje: "Error al modificar el perfil" });
+  }
 });
 
-// Ruta para agregar productos al carrito de compras
-app.post('/carrito', (req, res) => {
+app.post("/carrito", async (req, res) => {
   const { id_usuario, id_producto } = req.body;
-  const query = {
-    text: 'INSERT INTO CarritoCompras (id_usuario, id_producto) VALUES ($1, $2)',
-    values: [id_usuario, id_producto],
-  };
-
-  pool.query(query, (error) => {
-    if (error) {
-      console.error('Error al agregar producto al carrito:', error);
-      res.status(500).json({ mensaje: 'Error al agregar producto al carrito' });
-    } else {
-      res.json({ mensaje: 'Producto agregado al carrito' });
-    }
-  });
+  try {
+    await addToCart(id_usuario, id_producto);
+    res.json({ mensaje: "Producto agregado al carrito" });
+  } catch (error) {
+    console.error("Error al agregar producto al carrito:", error);
+    res.status(500).json({ mensaje: "Error al agregar producto al carrito" });
+  }
 });
 
-// Ruta para acceder a las cotizaciones guardadas por el usuario
-app.get('/usuarios/:id/cotizaciones', (req, res) => {
+app.get("/usuarios/:id/cotizaciones", async (req, res) => {
   const { id } = req.params;
-  const query = {
-    text: 'SELECT * FROM Compras WHERE id_usuario = $1',
-    values: [id],
-  };
-
-  pool.query(query, (error, result) => {
-    if (error) {
-      console.error('Error al obtener las cotizaciones:', error);
-      res.status(500).json({ mensaje: 'Error al obtener las cotizaciones' });
-    } else {
-      res.json(result.rows);
-    }
-  });
+  try {
+    const result = await getCotizations(id);
+    res.json(result);
+  } catch (error) {
+    console.error("Error al obtener las cotizaciones:", error);
+    res.status(500).json({ mensaje: "Error al obtener las cotizaciones" });
+  }
 });
 
-// Ruta para adjuntar la receta de lentes para cotizar
-app.post('/usuarios/:id/receta', (req, res) => {
+app.post("/usuarios/:id/receta", async (req, res) => {
   const { id } = req.params;
   const { receta } = req.body;
-  const query = {
-    text: 'UPDATE Usuarios SET receta = $1 WHERE id = $2',
-    values: [receta, id],
-  };
-
-  pool.query(query, (error) => {
-    if (error) {
-      console.error('Error al adjuntar la receta:', error);
-      res.status(500).json({ mensaje: 'Error al adjuntar la receta' });
-    } else {
-      res.json({ mensaje: 'Receta adjuntada correctamente' });
-    }
-  });
+  try {
+    await attachPrescription(id, receta);
+    res.json({ mensaje: "Receta adjuntada correctamente" });
+  } catch (error) {
+    console.error("Error al adjuntar la receta:", error);
+    res.status(500).json({ mensaje: "Error al adjuntar la receta" });
+  }
 });
 
 app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+  console.log("Servidor escuchando en el puerto 3000");
 });
---
+++ database/database.js
@@ -0,0 +1,148 @@
+const pg = require("pg");
+
+const pool = new pg.Pool({
+  user: "turyvzwx",
+  host: "rajje.db.elephantsql.com",
+  database: "turyvzwx",
+  password: "L1hFyXi3D7btq3Js7T5PJojIa9X-hRBA",
+  port: 5432, // ElephantSQL uses port 5432 by default
+});
+
+// Query function for generic queries
+const query = (queryString, values) => {
+  return new Promise((resolve, reject) => {
+    pool.query(queryString, values, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.rows);
+      }
+    });
+  });
+};
+
+// Functions for querying specific tables
+const getUsuarios = () => query("SELECT * FROM Usuarios;");
+const getProductos = () => query("SELECT * FROM Productos;");
+const getCarritoComprasByUser = (userId) =>
+  query("SELECT * FROM CarritoCompras WHERE id_usuario = $1;", [userId]);
+const getComprasByUser = (userId) =>
+  query("SELECT * FROM Compras WHERE id_usuario = $1;", [userId]);
+const getInventario = () => query("SELECT * FROM Inventario;");
+const getPromociones = () => query("SELECT * FROM Promociones;");
+const getResenasByUser = (userId) =>
+  query("SELECT * FROM Resenas WHERE id_usuario = $1;", [userId]);
+const getNotificacionesByUser = (userId) =>
+  query("SELECT * FROM Notificaciones WHERE id_usuario = $1;", [userId]);
+const getMetodoPagoByUser = (userId) =>
+  query("SELECT * FROM MetodoPago WHERE id_usuario = $1;", [userId]);
+const getContactoByUser = (userId) =>
+  query("SELECT * FROM Contacto WHERE id_usuario = $1;", [userId]);
+const getHistorialComprasByUser = (userId) =>
+  query("SELECT * FROM HistorialCompras WHERE id_usuario = $1;", [userId]);
+const getBusquedaProductoByUser = (userId) =>
+  query("SELECT * FROM BusquedaProducto WHERE id_usuario = $1;", [userId]);
+const getListaDeseosByUser = (userId) =>
+  query("SELECT * FROM ListaDeseos WHERE id_usuario = $1;", [userId]);
+
+const searchProducts = (tipo_lente, color, marca) => {
+  let queryString;
+  let values;
+  if (!tipo_lente || !color || !marca) {
+    queryString = "SELECT * FROM Productos";
+  } else {
+    queryString =
+      "SELECT * FROM Productos WHERE tipo_lente = $1 AND color = $2 AND marca = $3";
+    values = [tipo_lente, color, marca];
+  }
+  return query(queryString, values);
+};
+
+const searchProductsByNameOrDescription = (q) => {
+  return query(
+    "SELECT * FROM Productos WHERE nombre_producto ILIKE $1 OR descripcion ILIKE $1",
+    [`%${q}%`]
+  );
+};
+
+const userLogin = (nombre_usuario, contrasena) => {
+  return query(
+    "SELECT * FROM Usuarios WHERE nombre_usuario = $1 AND contrasena = $2",
+    [nombre_usuario, contrasena]
+  );
+};
+
+const updateUser = (
+  id,
+  nombre_usuario,
+  tipo_usuario,
+  contrasena,
+  email,
+  informacion_contacto
+) => {
+  return query(
+    "UPDATE Usuarios SET nombre_usuario = $1, tipo_usuario = $2, contrasena = $3, email = $4, informacion_contacto = $5 WHERE id = $6",
+    [nombre_usuario, tipo_usuario, contrasena, email, informacion_contacto, id]
+  );
+};
+
+const createUser = (
+  nombre_usuario,
+  tipo_usuario,
+  contrasena,
+  email,
+  nombre,
+  apellido
+) => {
+  console.log(
+    nombre_usuario,
+    tipo_usuario,
+    contrasena,
+    email,
+    nombre,
+    apellido
+  );
+  return query(
+    "INSERT INTO Usuarios (nombre_usuario, tipo_usuario, contrasena, email, nombre, apellido) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [nombre_usuario, tipo_usuario, contrasena, email, nombre, apellido]
+  ).then((result) => result[0]);
+};
+
+const addToCart = (id_usuario, id_producto) => {
+  return query(
+    "INSERT INTO CarritoCompras (id_usuario, id_producto) VALUES ($1, $2)",
+    [id_usuario, id_producto]
+  );
+};
+
+const getCotizations = (id) => {
+  return query("SELECT * FROM Compras WHERE id_usuario = $1", [id]);
+};
+
+const updateRecipe = (id, receta) => {
+  return query("UPDATE Usuarios SET receta = $1 WHERE id = $2", [receta, id]);
+};
+module.exports = {
+  query,
+  searchProducts,
+  searchProductsByNameOrDescription,
+  userLogin,
+  createUser,
+  updateUser,
+  addToCart,
+  getCotizations,
+  updateRecipe,
+  getUsuarios,
+  getProductos,
+  getCarritoComprasByUser,
+  getComprasByUser,
+  getInventario,
+  getPromociones,
+  getResenasByUser,
+  getNotificacionesByUser,
+  getMetodoPagoByUser,
+  getContactoByUser,
+  getHistorialComprasByUser,
+  getBusquedaProductoByUser,
+  getListaDeseosByUser,
+};
